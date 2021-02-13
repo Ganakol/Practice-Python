4e61dbcc629c17e460b52b76a4c006f484ff86c7
Ganakol <70902763+Ganakol@users.noreply.github.com>
2021-02-13 20:56:05 +0530

    Update Default_Argument.py

--- Default_Argument.py
+++ Default_Argument.py
@@ -20,6 +20,16 @@ def print_from_stream(n, stream=EvenStream()):
     stream.__init__()
     for _ in range(n):
         print(stream.get_next())
+        
+"""def print_from_stream(n, stream=None):
+    if stream == None:
+        stream = EvenStream()
+        for _ in range(n):
+            print(stream.get_next())
+    else:
+        stream = OddStream()
+        for _ in range(n):
+            print(stream.get_next())"""
 
 
 queries = int(input())
